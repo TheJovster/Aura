7b1d8db275a1174a0834f29fd18927ee8ab9a75c
TheJovster <joca.251@hotmail.com>
2024-04-06 13:31:46 +0200

    Moved back the AuraPlayerCharacter and AuraBaseCharacter classes to their roots

---
+++ Source/Aura/Private/AuraCharacterBase.cpp
@@ -0,0 +1,27 @@
+#include "Characters/AuraCharacterBase.h"
+
+// Sets default values
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AAuraCharacterBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AAuraCharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
