0d90430f434e6eadfa24e277809185d017f455ba
TheJovster <joca.251@hotmail.com>
2024-04-06 13:18:37 +0200

    Added AuraCharacterBase and AuraPlayerCharacter classes

---
+++ Source/Aura/Private/AuraPlayerCharacter.cpp
@@ -0,0 +1,5 @@
+
+
+
+#include "AuraPlayerCharacter.h"
+
---
+++ Source/Aura/Public/AuraPlayerCharacter.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraCharacterBase.h"
+#include "AuraPlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraPlayerCharacter : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+	
+};
